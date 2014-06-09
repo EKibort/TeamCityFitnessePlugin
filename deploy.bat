131c635bcee8a97a543222fbffd2b259105cf890
Eduard Kibort <advardtwo@gmail.com>
2014-06-09 22:41:16 +0400

    Use TEAMCITY_DATA_PATH variable insted of USERPROFILE in deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
+REM SET TEAMCITY_HOME=C:\TeamCity
 call ant
-copy .\dist\Fitnesse.zip "%USERPROFILE%\.BuildServer\plugins\"  /Y
+copy .\dist\Fitnesse.zip "%TEAMCITY_DATA_PATH%\plugins\"  /Y
 call %TEAMCITY_HOME%\buildAgent\bin\agent.bat stop force
 CHOICE /C:AB /D:A /T:2 >NUL
 call %TEAMCITY_HOME%\buildAgent\bin\agent.bat start
